c500ca075893145f9b3cbed58b337563726bdd48
arockia1 <arockia1@f6607d7f-6149-4549-8ee8-9fbe3f9dfa8c>
2016-02-18 15:34:22 +0000

    result comparator added

--- kendaltau_distance.py
+++ kendaltau_distance.py
@@ -22,44 +22,44 @@ for record in f:
       dst_node = int(record[1])
       neighbors.setdefault(src_node,set()).add(dst_node)
 
+if __name__ == '__main__':
+  role_list = {}
+  role = {}
+  degree_list = dict()
+  f = open(res_filename, 'r+')
+  for record in f:
+      record= record.strip().split('\t')
+      node = int(record[0])
+      node_role = int(record[1])
+      role[node]=node_role
+      role_list.setdefault(node_role,set()).add(node)
+  avg_degree = {}
 
-role_list = {}
-role = {}
-degree_list = dict()
-f = open(res_filename, 'r+')
-for record in f:
-    record= record.strip().split('\t')
-    node = int(record[0])
-    node_role = int(record[1])
-    role[node]=node_role
-    role_list.setdefault(node_role,set()).add(node)
-avg_degree = {}
+  for keys in role_list:
+    nodes = list(role_list[keys])
+    nodes.sort()
+    deg_sum = 0
+    for node in nodes:
+      if node in neighbors:
+	deg_sum += len(neighbors[node])
+	degree_list[node] = len(neighbors[node])
+      else:
+	deg_sum += 0
+	degree_list[node] = 0       
+    avg_degree[keys] = deg_sum/float(len(nodes))
+  sorted_role = sorted(avg_degree.items(), key=operator.itemgetter(1))
 
-for keys in role_list:
-  nodes = list(role_list[keys])
-  nodes.sort()
-  deg_sum = 0
-  for node in nodes:
-    if node in neighbors:
-       deg_sum += len(neighbors[node])
-       degree_list[node] = len(neighbors[node])
-    else:
-       deg_sum += 0
-       degree_list[node] = 0       
-  avg_degree[keys] = deg_sum/float(len(nodes))
-sorted_role = sorted(avg_degree.items(), key=operator.itemgetter(1))
+  z = zip(*sorted_role)
+  sorted_role_list = np.argsort(list(z[0]))
 
-z = zip(*sorted_role)
-sorted_role_list = np.argsort(list(z[0]))
+  print sorted_role_list
+  role1 = deepcopy(role)
+  for keys in role:
+    role1[keys]=sorted_role_list[role[keys]]
 
-print sorted_role_list
-role1 = deepcopy(role)
-for keys in role:
-  role1[keys]=sorted_role_list[role[keys]]
+  x = list(role1.values())
+  y = list(degree_list.values())
 
-x = list(role1.values())
-y = list(degree_list.values())
-
-tau, p_value = stats.kendalltau(x, y)
-print 'TAU:',tau
-print 'P_VALUE:',p_value
+  tau, p_value = stats.kendalltau(x, y)
+  print 'TAU:',tau
+  print 'P_VALUE:',p_value
