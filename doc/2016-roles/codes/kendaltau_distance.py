9ba1969587c0fda4a907be4adf574691501b954a
arockia1 <arockia1@f6607d7f-6149-4549-8ee8-9fbe3f9dfa8c>
2016-02-18 15:30:11 +0000

    README is added

--- doc/2016-roles/codes/kendaltau_distance.py
+++ doc/2016-roles/codes/kendaltau_distance.py
@@ -30,7 +30,6 @@ f = open(res_filename, 'r+')
 for record in f:
     record= record.strip().split('\t')
     node = int(record[0])
-    #degree = int(record[1])
     node_role = int(record[1])
     role[node]=node_role
     role_list.setdefault(node_role,set()).add(node)
@@ -47,36 +46,20 @@ for keys in role_list:
     else:
        deg_sum += 0
        degree_list[node] = 0       
-  #print deg_sum
   avg_degree[keys] = deg_sum/float(len(nodes))
-#print avg_degree
 sorted_role = sorted(avg_degree.items(), key=operator.itemgetter(1))
-#print sorted_role
 
 z = zip(*sorted_role)
 sorted_role_list = np.argsort(list(z[0]))
-#print sorted_role_list
-#sl =[8]
-#for x in sorted_role_list:
-#   sl.append(x)
-#sorted_role_list.append(5)
-#sorted_role_list.append(8)
-#sorted_role_list.append(3)
-#sorted_role_list = deepcopy(sl)
+
 print sorted_role_list
 role1 = deepcopy(role)
 for keys in role:
   role1[keys]=sorted_role_list[role[keys]]
-#print role
-#print degree_list
+
 x = list(role1.values())
 y = list(degree_list.values())
-#print role
-#print role1
-#print np.mean([degree_list[z] for z in role1 if role1[z] == 0])
-#print np.mean([degree_list[z] for z in role1 if role1[z] == 1])
-#print np.mean([degree_list[z] for z in role1 if role1[z] == 2])
-#print np.mean([degree_list[z] for z in role1 if role1[z] == 3])
+
 tau, p_value = stats.kendalltau(x, y)
 print 'TAU:',tau
 print 'P_VALUE:',p_value
